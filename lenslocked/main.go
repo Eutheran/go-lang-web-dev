4d9f5492225846e2ea638c86e66d040159771dfa
Eric K <kreitere@gmail.com>
2023-01-22 21:37:26 -0500

    refact: refactored routes to chi router

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,38 +34,18 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	`)
 }
 
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		http.Error(w, "Page Not Found", http.StatusNotFound)
-// 	}
-// }
-
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "Page Not Found", http.StatusNotFound)
-	}
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Page not found", http.StatusNotFound)
 }
 
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(notFoundHandler)
 	fmt.Println("Starting the server on :3000...")
-	// we are not passing the pathHandler into a function
-	// this http.HandlerFunc is converting the handler type
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
 
 // Notes:
